0f37d583927693376a34d9416913236662559ce6
jakesavi <jws2460@gmail.com>
2024-12-02 16:59:23 -0500

    Added Python Lazy version for D1P1

--- Day1/PythonLazy/QuicknEZ.py
+++ Day1/PythonLazy/QuicknEZ.py
@@ -1,7 +1,24 @@
 
+def main():
+    with open("InputData.txt") as file:
+        leftColumn = []
 
+        rightColumn = []
 
-main():
-    with open(reader) as file: 
+        DistanceToSum = []
         for i in file:
-            i.split()
+            lineList = i.strip().split("   ")
+            leftColumn.append(int(lineList[0]))
+            rightColumn.append(int(lineList[1]))
+            
+        #Actual procedure below
+        while(len(leftColumn) > 0 and len(rightColumn) > 0  ):
+
+            smallestL = min(leftColumn)
+            smallestR = min(rightColumn)
+            DistanceToSum.append(abs(smallestL-smallestR)) 
+            leftColumn.pop(leftColumn.index(smallestL))
+            rightColumn.pop(rightColumn.index(smallestR))
+
+        print(sum(DistanceToSum))
+main()
